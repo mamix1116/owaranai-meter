fcfd6747f76e3b98c86789e59810646aafb8952f
Kazuki Imamura <kaizumaki@gmail.com>
2021-02-07 22:14:55 +0900

    BootstrapVueのscssを上書きしてカスタマイズ

---
+++ frontend/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
+}
