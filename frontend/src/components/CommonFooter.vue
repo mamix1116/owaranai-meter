2717c2408e82afa1db33329620f51c65e04d4775
Kazuki Imamura <kaizumaki@gmail.com>
2021-02-08 21:52:34 +0900

    フッターコンポーネントを追加

---
+++ frontend/src/components/CommonFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="mt-4 py-2">
+    <div class="text-center">
+      <small>&copy; 2021 OWARANAI METER</small>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "CommonFooter"
+};
+</script>
