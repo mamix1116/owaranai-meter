ae317b9d69238713a0adf7c156e549f0b9ba141e
Kazuki Imamura <kaizumaki@gmail.com>
2021-02-10 00:12:30 +0900

    githubリポジトリへのリンクを追加

--- frontend/src/components/CommonFooter.vue
+++ frontend/src/components/CommonFooter.vue
@@ -3,6 +3,15 @@
     <div class="text-center">
       <small>&copy; 2021 OWARANAI METER</small>
     </div>
+    <div class="text-center">
+      <a
+        href="https://github.com/mamix1116/owaranai-meter"
+        rel="nofollow"
+        target="_blank"
+      >
+        <b-icon icon="github" aria-hidden="true"></b-icon> GitHub
+      </a>
+    </div>
   </footer>
 </template>
 
