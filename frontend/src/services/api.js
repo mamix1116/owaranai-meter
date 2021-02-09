b73b9e22eddd584b199ccf3106d63a9235dd8498
Kazuki Imamura <kaizumaki@gmail.com>
2021-02-10 02:33:38 +0900

    csrftokenの設定を追加

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+
 const api = axios.create({
   baseURL: process.env.VUE_APP_ROOT_API,
   timeout: 5000,
