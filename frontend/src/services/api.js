07abf0de7b431495baddd5a2a73fcb1ca47b67e0
Hiroshi Omata <op.homata@gmail.com>
2021-02-10 02:58:56 +0000

    Update docker-compose.production nginx port

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = false;
-axios.defaults.xsrfCookieName = "csrftoken";
-axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-
 const api = axios.create({
   baseURL: process.env.VUE_APP_ROOT_API,
   timeout: 5000,
