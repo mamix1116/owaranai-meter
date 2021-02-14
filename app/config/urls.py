4ce4433c7ccdaac674af044441d2613f3f62dae9
Hiroshi Omata <op.homata@gmail.com>
2021-02-14 09:39:30 +0900

    clean up comment out code in urls.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -27,10 +26,7 @@ admin.site.index_title = 'データ'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('meetings/', include(meetings.urls)),
     path('api/<version>/', include('meetings.api.urls')),
-    # path('', TemplateView.as_view(template_name='index.html')),
-    # path('try_meter/', TemplateView.as_view(template_name='try_meter.html')),
 ]
 
 if bool(settings.DEBUG):
