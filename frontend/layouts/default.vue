bf99e5d863450dd67e12c7a3e0ff71c645719f30
Kazuki Imamura <kaizumaki@gmail.com>
2021-02-25 10:08:50 +0900

    layoutにmetaタグを設定

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -11,8 +11,45 @@ import CommonFooter from '@/components/CommonFooter.vue'
 
 export default Vue.extend({
   components: {
-    CommonFooter
-  }
+    CommonFooter,
+  },
+  head() {
+    return {
+      title: this.$tc('title'),
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.$tc('lead'),
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: this.$tc('title'),
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.$tc('lead'),
+        },
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: this.$tc('title'),
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://owaranai.tokyo/ogp.png',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `https://owaranai.tokyo${this.$route.path}`,
+        },
+      ],
+    }
+  },
 })
 </script>
 
