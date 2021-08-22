d90bd32a25f6238cfefa88b1ced95582ba23306c
Kazuki Imamura <kaizumaki@gmail.com>
2021-08-22 16:21:14 +0900

    drawChartPluginをnuxt.configに登録

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   css: ['@/assets/scss/global.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/api.ts'],
+  plugins: ['@/plugins/api.ts', '@/plugins/drawChartPlugin.ts'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
